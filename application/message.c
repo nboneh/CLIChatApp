776d1351ccd7fc37e8a11e1a980555ca422487a3
nir.boneh@colroado.edu <nboneh5921@gmail.com>
2016-04-21 20:03:50 -0600

    Starting work on add command

--- application/message.c
+++ application/message.c
@@ -9,10 +9,28 @@
 #include <string.h>
 #include <signal.h>
 
+void  parseCmd(char *cmd, char **argv)
+{    
+   //Spliting basd on white spaces
+   //While not end of cmd
+     while (*cmd != '\0') {   
+        //Replace all white spaces with 0, this will act as seperator between argv strings
+          while (*cmd == ' ' || *cmd == '\t' || *cmd == '\n')  
+               *cmd++ = '\0'; 
+           //As soon as we reach a character that's not space add its position to argv
+          *argv++ = cmd; 
+          //Keep cycling until reach next white space or reach end of cmd  or still insideParenthesis    
+          while (*cmd != '\0' && *cmd != ' ' && 
+                 *cmd != '\t' && *cmd != '\n'){
+               cmd++;   
+          }
+     } 
+     //Adding terminating 0 to end of argument list
+     *argv = '\0';                
+}
 
+void add(char *IP, char *contactname){
 
-void interactiveMode(){
- 
 }
 
 int main() {
@@ -34,19 +52,41 @@ int main() {
     if ((strlen(cmd)>0) && (cmd[strlen (cmd) - 1] == '\n'))
         cmd[strlen (cmd) - 1] = '\0';
 
-
-    //Exiting on quit command
-    if(strcmp(cmd,"quit") == 0){
-      printf("Goodbye\n");
-       break;
-     }
-    
     //Exiting on 'Ctrl-D'
     if(res == NULL){
       printf("\n");
       printf("Goodbye\n");
       break;
     }
+    if(strlen(cmd) >= 3 && cmd[0] == 'a' && cmd[1] == 'd' && cmd[2] == 'd'){
+      //add contact
+      int argv_size = 3;
+      char *argv[argv_size];
+      argv[1] = NULL;
+      argv[2] = NULL;
+      parseCmd(cmd, argv);
+
+      if(argv[1] == NULL || argv[2] == NULL){
+        printf("Invalid use of add command, type help for list of commands\n");
+        continue;
+      }
+      add(argv[1], argv[2]);
+
+    } else if(strcmp(cmd, "help") == 0){
+      //help
+      printf("Commands:\n");
+      printf("message (IP/contactname) - start messaging with the specific IP or contanct\n");
+      printf("add (IP) (contactname) - this will add a new contact based on IP address\n");
+      printf("delete (contactname) - allows you remove a contanct\n");
+      printf("quit or Ctrl-D - in order to exit the application\n");
+    } else if(strcmp(cmd,"quit") == 0){
+      //Exiting on quit command
+      printf("Goodbye\n");
+      break;
+    } else{
+      printf("Invalid command, type help for list of commands\n");
+    }
+    
 
   } 
   return 0; 
