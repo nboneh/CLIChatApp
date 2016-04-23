e54b63587d0044ebf949cdf78e1db5d1af53b588
Nir Boneh <nboneh5921@gmail.com>
2016-04-22 21:30:43 -0600

    Making bash work for mac

--- application/myip.sh
+++ application/myip.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 printf  "LAN "
-echo $(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
+if [  "$(uname)" == "Darwin"  ]
+	then
+    # Mac
+    ifconfig en1 | awk '{ print $2}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
+else  
+    # Linux
+    echo $(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
+fi
 printf  "WAN "
 curl -s http://whatismijnip.nl |cut -d " " -f 5
