14e24c5bf3326c3a0d0c28f225ea3b6d8a8192c6
nir.boneh@colroado.edu <nboneh5921@gmail.com>
2016-04-22 21:21:09 -0600

    Testing sockets

--- application/myip.sh
+++ application/myip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 printf  "LAN "
-hostname --ip-address
+echo $(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
 printf  "WAN "
 curl -s http://whatismijnip.nl |cut -d " " -f 5
